abab17ddd9d066289b5261352ca0a789f9bcfc79
LavanyaAnumula <lavanyaanumula0511@gmail.com>
2026-03-16 01:52:33 +0530

    Add files via upload

---
+++ CloudProjectFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloudsecure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudProjectFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
